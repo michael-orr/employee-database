89704c96011f54c25d664fc53f0327a0c9711dc3
Michael Orr <mike@glquote.com>
2021-10-26 16:46:19 -0700

    removed routing, view functions and add department function working

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
---
+++ index.js
@@ -0,0 +1,155 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const art = require("ascii-art");
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Sloane2022?',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+  async function start() {
+    try {
+      const rendered = await art.font("Employee Manager", "doom").completed();
+      console.log(rendered);
+      return menu();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function menu() {
+    const { action } = await inquirer.prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "action",
+        choices: ["View All Employees", "View All Departments", "View All Roles", "Add Employee", "Add Department", "Add Role", "Update Employee Role", "Exit"],
+      },
+    ]);
+  
+    switch (action) {
+      case "View All Employees":
+        return viewEmployees();
+  
+      case "View All Departments":
+        return viewDepartments();
+  
+      case "View All Roles":
+        return viewRoles();
+      
+      case "Update Employee Role":
+        return updateEmployeeRole();
+
+      case "Add Department":
+        return addDepartment();  
+
+      case "Add Role":
+        return addRole();
+
+      case "Add Employee":
+        return addEmployee();     
+
+      default:
+        return exit();
+    }
+  }
+
+  function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+      console.table(results);
+      menu();
+    }); 
+  }
+
+  function viewRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+      console.table(results);
+      menu();
+    }); 
+  }
+  function viewDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+      console.table(results);
+      menu();
+    }); 
+  }
+  function updateEmployeeRole() {
+    console.log('You chose Update Employee.');
+    menu();
+  }
+  async function addDepartment() {
+    try{
+    const { department } = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What would you like to name the department?",
+        name: "department",
+      },
+    ]);
+    db.query(`INSERT INTO department (name) VALUES (?);`, department, function (err, results) {
+      console.log(`The ${department} Department has been successfully added.`);
+    }); 
+    db.query('SELECT * FROM department', function (err, results) {
+      console.table(results);
+      menu();
+    });
+  } catch (error) {
+    console.log(error);
+  }  
+ }
+ async function addRole() {
+  try{
+  const { role } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What would you like to name this role?",
+      name: "role",
+    },
+  ]);
+  db.query(`INSERT INTO department (name) VALUES (?);`, role, function (err, results) {
+    console.log(`${role} has been successfully added.`);
+  }); 
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results);
+    menu();
+  });
+} catch (error) {
+  console.log(error);
+}  
+}
+async function addEmployee() {
+  try{
+  const { department } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employees first name?",
+      first_name: "first_name",
+    },
+    {
+      type: "input",
+      message: "What is the employees last name?",
+      first_name: "last_name",
+    },
+  ]);
+  db.query(`INSERT INTO department (name) VALUES (?);`, department, function (err, results) {
+    console.log(`The ${department} Department has been successfully added.`);
+  }); 
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results);
+    menu();
+  });
+} catch (error) {
+  console.log(error);
+}  
+}
+  function exit() {
+    console.log('see ya!');
+    db.end();
+  }
+
+  start();
